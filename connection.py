7015f46754d973936fad627e319baca5dab355af
eenblam <eenblam@riseup.net>
2023-04-05 17:36:59 -0700

    Add connection.py

---
+++ connection.py
@@ -0,0 +1,21 @@
+"""
+Connection is just a wrapper around pymysql.Connection
+to provide preferable cursor defaults throughout the project.
+"""
+
+import pymysql
+
+class Connection(pymysql.Connection):
+    # We get __enter__() for free, just want it to call __init__
+    def __init__(self, config, *args, **kwargs):
+        super().__init__(host=config.host,
+                       user=config.user,
+                       password=config.password,
+                       database=config.database,
+                       *args, **kwargs)
+
+    def cursor(self, cursor=pymysql.cursors.DictCursor, **kwargs):
+        """Ensure ANSI_QUOTES, default to DictCursor"""
+        c = super().cursor(cursor=cursor, **kwargs)
+        c.execute('SET SQL_MODE=ANSI_QUOTES')
+        return c

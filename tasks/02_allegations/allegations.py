084a5c8adb6ef592db4df6f8f1fa1290de8ad44c
eenblam <eenblam@riseup.net>
2023-04-11 14:04:04 -0700

    Move main function to task.py
    * Parser code is still in allegations.py
    * Changed the make target from "main" to "task"

--- tasks/02_allegations/allegations.py
+++ tasks/02_allegations/allegations.py
@@ -1,11 +1,5 @@
-#!/usr/bin/env python3
-
-from common import db_config, Connection
-
 from collections import namedtuple
 import re
-
-import pymysql
 
 # Match cases like 8(a)(1) and 8(a)(1)(A)
 # '(?:\([A-Z]+\))?' is an optional non-matching group for the last (A)
@@ -66,36 +60,4 @@ def process_allegations(cursor, case_row):
             raise(exc)
 
 
-def main():
-    """Run the migration."""
 
-    allegations_query = """
-    SELECT id, case_number, allegations_raw
-    FROM cases
-    WHERE allegations_raw IS NOT NULL
-    AND allegations_raw <> ''
-    AND allegations_parse_error IS NULL
-    ;
-    """
-
-    cnx = Connection(db_config)
-    cnx.begin()
-    try:
-        c = cnx.dict_cursor()
-        n = c.execute(allegations_query)
-        print(f'Cases with allegations: {n}')
-        for row in c:
-            process_allegations(cnx.dict_cursor(), row)
-        cnx.commit()
-        print('Migration complete.')
-    except Exception as e:
-        print(f'Error: {e}')
-        print('Rolling back.')
-        cnx.rollback()
-    finally:
-        c.close()
-        cnx.close()
-
-
-if __name__ == '__main__':
-    main()

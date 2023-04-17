dc61e51b372eba9ac1f6ed4fbd6d5123bb42bd10
Nikki Everett <n.everett@gmail.com>
2023-04-17 15:17:08 -0500

    remove paths from this file, since we have a paths config file now

--- Common/app_config.py
+++ Common/app_config.py
@@ -1,10 +1,4 @@
-# commonly used directories
-input_dir = '../../data/case_files/'
-output_dir = ''
-sql_dir = '../../sql/'
-
 # schema name and commonly used database tables
-# TODO maybe make this a dict with table name and create sql filename?
 schema = 'nlrb_data'
 cases_raw = 'cases_raw'
 cases_raw_deduped = 'cases_raw_deduped'

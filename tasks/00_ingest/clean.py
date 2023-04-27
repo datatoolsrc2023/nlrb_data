315326fac6522a7d1772e69f088199b6ddfd92fc
Nikki Everett <n.everett@gmail.com>
2023-04-27 15:32:27 -0500

    refactored sql connection

--- tasks/00_ingest/clean.py
+++ tasks/00_ingest/clean.py
@@ -7,14 +7,22 @@ import sys
 if __name__ == '__main__':
     """Drop cases_raw table"""
 
-    with sql.db_cnx() as cnx, cnx.cursor() as c:
-        query = f"""
-                DROP TABLE IF EXISTS
-                {db_config.cases_raw}
-                """
-        try:
-            print(f'Dropping {db_config.cases_raw} table')
-            c.execute(query)
-        except Exception as e:
-            print(f"Failed to drop {db_config.cases_raw}: {e}")
-            sys.exit(1)
+    error = False
+
+    cnx = sql.db_cnx()
+    c = cnx.cursor()
+    query = f"""
+            DROP TABLE IF EXISTS
+            {db_config.cases_raw}
+            """
+    try:
+        print(f'attempting to drop {db_config.cases_raw} table')
+        c.execute(query)
+    except:
+        print("failed to drop table")
+        raise
+    else: # no exception
+        cnx.commit()
+        print("dropped {db_config.cases_raw} table")
+    finally:
+        cnx.close()

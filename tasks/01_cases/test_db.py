f14b880081fc7cd456358ff656bab3a53579068d
Nikki Everett <n.everett@gmail.com>
2023-05-10 16:24:05 -0500

    update cases task to accommodate sqlite

--- tasks/01_cases/test_db.py
+++ tasks/01_cases/test_db.py
@@ -4,20 +4,29 @@ from common import db_config, sql
 if __name__ == '__main__':
     """Confirm cases table exists."""
 
-    query = f"""
-            SELECT * FROM pg_tables
-            WHERE tablename = 'cases';
-            """
+    if db_config.db_type == 'sqlite3':
+        query = f"""
+                SELECT name FROM sqlite_master
+                WHERE type='table'
+                AND name='cases';
+                """
+    elif db_config.db_type == 'postgresql':
+        query = f"""
+                SELECT * FROM pg_tables
+                WHERE tablename = 'cases';
+                """
 
     try:
-        with sql.db_cnx() as cnx, cnx.cursor() as c:
+        with sql.db_cnx() as cnx:
+            c = cnx.cursor()
             c.execute(query)
-            if not c.fetchone():
-                raise Exception(f'Expected {db_config.cases} to exist, '
-                                'but table does not exist')
     except Exception as e:
         raise Exception(f'Could not test for existence of {db_config.cases} table') from e
     else:
+        if not c.fetchone():
+            raise Exception(f'Expected {db_config.cases} to exist, '
+                            'but table does not exist')
         print(f'{db_config.cases} table exists')
     finally:
+        c.close()
         cnx.close()
